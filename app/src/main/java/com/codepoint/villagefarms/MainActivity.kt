b5b528602a24aad718d5875687346b12d5ea6729
nathanbaleeta <nbaleeta@gmail.com>
2019-08-07 22:34:01 +0300

    Added register farmer activity and create farmer form

--- app/src/main/java/com/codepoint/villagefarms/MainActivity.kt
+++ app/src/main/java/com/codepoint/villagefarms/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.codepoint.villagefarms
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +15,6 @@ class MainActivity : AppCompatActivity() {
         when (item.itemId) {
             R.id.navigation_farmers -> {
                 textMessage.setText(R.string.title_farmers)
-
-
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_advances -> {
@@ -35,9 +36,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         textMessage = findViewById(R.id.message)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        // Floating Action Button for farmer activity
+        val fab: View = findViewById(R.id.fabFarmer)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Farmer was successfully registered", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show()
+
+            // Open register farmer activity from farmer list activity
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+
+        }
+
+
+
     }
 }
---
+++ app/src/main/java/com/codepoint/villagefarms/RegisterActivity.kt
@@ -0,0 +1,22 @@
+package com.codepoint.villagefarms
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+
+class RegisterActivity : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+
+
+
+
+    }
+
+
+}
