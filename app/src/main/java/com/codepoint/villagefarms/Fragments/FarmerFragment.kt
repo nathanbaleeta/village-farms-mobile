420873b607228f9c08023d91a301506a2ff53e49
nathanbaleeta <nbaleeta@gmail.com>
2019-08-28 12:52:39 +0300

    Added feature to start activity intents from within fragments like farmer

--- app/src/main/java/com/codepoint/villagefarms/Fragments/FarmerFragment.kt
+++ app/src/main/java/com/codepoint/villagefarms/Fragments/FarmerFragment.kt
@@ -1,11 +1,14 @@
 package com.codepoint.villagefarms.Fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.codepoint.villagefarms.R
+import com.codepoint.villagefarms.RegisterActivity
+import kotlinx.android.synthetic.main.fragment_farmer.*
 
 class FarmerFragment : Fragment(){
 
@@ -15,7 +18,16 @@ class FarmerFragment : Fragment(){
         return inflater.inflate(R.layout.fragment_farmer, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
+        // Start registration activity from farmer fragment
+        fabRegister.setOnClickListener {
+            val intent = Intent(activity, RegisterActivity::class.java)
+            activity?.startActivity(intent)
+        }
+
+    }
 
 
     companion object {
